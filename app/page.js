6e0e1b112fda7e9e8db2ae933833f88fc1a0a629
dismostore <application@dismo.co.uk>
2024-06-26 23:59:09 +0100

    init

--- app/page.js
+++ app/page.js
@@ -3,15 +3,13 @@ import { linkVariants } from "@/components/ui/link";
 
 export default function Home() {
   return (
-    <main>
-      <div className="m-2">
-        <a href="" className={linkVariants({ variant: "default" })}>
-          Click here
-        </a>
-        <a href="" className={buttonVariants({ variant: "default" })}>
-          Click here
-        </a>
-      </div>
-    </main>
+    <div className="m-2">
+      <a href="" className={linkVariants({ variant: "default" })}>
+        Click here
+      </a>
+      <a href="" className={buttonVariants({ variant: "default" })}>
+        Click here
+      </a>
+    </div>
   );
 }
